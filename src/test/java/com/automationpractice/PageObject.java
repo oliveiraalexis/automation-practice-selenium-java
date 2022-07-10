24449f01969e85119289495da3bc2c8cb77c200f
Aléxis Oliveira <alexis.silvaoliveira@gmail.com>
2022-07-10 16:30:46 -0300

    Criando classe PageObject

---
+++ src/test/java/com/automationpractice/PageObject.java
@@ -0,0 +1,29 @@
+package com.automationpractice;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PageObject {
+	
+protected WebDriver browser;
+	
+	public PageObject() { //construtor sem parâmetro
+		System.setProperty("webdriver.chrome.driver","src/test/resources/ChromeDriver103.0.5060.53/chromedriver.exe");
+		this.browser = new ChromeDriver();
+		this.browser.manage().timeouts()
+		.implicitlyWait(5, TimeUnit.SECONDS) //Esperar 5 segundos antes de lançar erro por não encontrar um elemento
+		.pageLoadTimeout(10, TimeUnit.SECONDS); //Esperar uma página carregar por 10 segundos antes de lancar erro
+	}
+	
+	public PageObject(WebDriver browser) { //construtor com parâmetro
+		System.setProperty("webdriver.chrome.driver","src/test/resources/ChromeDriver103.0.5060.53/chromedriver.exe");
+		this.browser = browser;
+	}
+	
+	public void fechar() {
+		this.browser.quit();
+	}
+
+}
