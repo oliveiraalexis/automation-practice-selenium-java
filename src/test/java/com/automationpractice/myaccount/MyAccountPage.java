58b66a41ace45b22ba2abddb1293c1db0650a65d
Aléxis Oliveira <alexis.silvaoliveira@gmail.com>
2022-07-10 19:14:37 -0300

    Criando classe da página My Account

---
+++ src/test/java/com/automationpractice/myaccount/MyAccountPage.java
@@ -0,0 +1,23 @@
+package com.automationpractice.myaccount;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import com.automationpractice.PageObject;
+
+public class MyAccountPage extends PageObject{
+	
+	private static final String URL_MYACCOUNT = "http://automationpractice.com/index.php?controller=my-account";
+	
+	public MyAccountPage (WebDriver browser) {
+		super(browser);
+	}
+	
+	public String getLoggedUsername() {
+		return browser.findElement(By.className("account")).getText();
+	}
+	
+	public Boolean isMyAccountPage() {
+		return browser.getCurrentUrl().equals(URL_MYACCOUNT);
+	}
+
+}
