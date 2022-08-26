ea3e100bdcb0a85eecc2029f9c6d2d5210b4caef
Aléxis Oliveira <alexis.silvaoliveira@gmail.com>
2022-08-25 21:18:26 -0300

    Create PersonalInformationTest.java

---
+++ src/test/java/com/automationpractice/myaccount/PersonalInformationTest.java
@@ -0,0 +1,20 @@
+package com.automationpractice.myaccount;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class PersonalInformationTest {
+	
+	private PersonalInformationPage paginaInformacoesPessoais;
+	
+	@BeforeEach
+	public void before() {
+		
+	}
+	
+	@AfterEach
+	public void after() {
+		
+	}
+
+}
