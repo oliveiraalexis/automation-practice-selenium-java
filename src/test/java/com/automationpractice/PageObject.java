4acf0d2efceeffd22481cc6e1da369691c3f0391
Aléxis Oliveira <alexis.silvaoliveira@gmail.com>
2022-08-25 21:51:27 -0300

    Rename chromedriver folder in PageObject class

--- src/test/java/com/automationpractice/PageObject.java
+++ src/test/java/com/automationpractice/PageObject.java
@@ -10,7 +10,7 @@ public class PageObject {
 protected WebDriver browser;
 	
 	public PageObject() { //construtor sem parâmetro
-		System.setProperty("webdriver.chrome.driver","src/test/resources/ChromeDriver103.0.5060.53/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","src/test/resources/ChromeDriver/chromedriver.exe");
 		this.browser = new ChromeDriver();
 		this.browser.manage().timeouts()
 		.implicitlyWait(5, TimeUnit.SECONDS) //Esperar 5 segundos antes de lançar erro por não encontrar um elemento
